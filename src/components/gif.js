8ee4e6fba0b854cf8c6e2f71ed03a29f23c8d6c7
Saken <sakenspot@gmail.com>
2025-03-08 15:52:35 +0700

    Update gif.js

--- src/components/gif.js
+++ src/components/gif.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let gif = document.createElement("img");
     gif.src = "https://media.giphy.com/media/WrfgK836swOvrdycaG/giphy.gif";
     gif.alt = "GIF Animasi";
-    gif.style.width = "50px"; // Sesuaikan ukuran GIF
+    gif.style.width = "90px"; // Sesuaikan ukuran GIF
     gif.style.position = "fixed"; // Agar tetap di satu posisi
-    gif.style.top = "10px"; // Jarak dari atas
-    gif.style.right = "10px"; // Jarak dari kanan
+    gif.style.top = "25px"; // Jarak dari atas
+    gif.style.right = "25px"; // Jarak dari kanan
     gif.style.pointerEvents = "none"; // Biar tidak menghalangi klik
     gif.style.zIndex = "9999"; // Pastikan di atas elemen lain
 
