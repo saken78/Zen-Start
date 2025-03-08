d8009ad608c06b56a4b3f23cfd46e768f649e7a4
Saken <sakenspot@gmail.com>
2025-03-08 15:03:11 +0700

    Update gif.js

--- src/components/tabs/gif.js
+++ src/components/tabs/gif.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let img = document.createElement("img");
-    img.src = "https://media.giphy.com/media/WrfgK836swOvrdycaG/giphy.gif";
-    img.alt = "GIF Animasi";
-    img.style.width = "200px";
-    img.style.display = "block";
-    img.style.margin = "auto";
+    let gif = document.createElement("img");
+    gif.src = "https://media.giphy.com/media/WrfgK836swOvrdycaG/giphy.gif";
+    gif.alt = "GIF Animasi";
+    gif.style.width = "50px"; // Sesuaikan ukuran GIF
+    gif.style.position = "absolute";
+    gif.style.pointerEvents = "none"; // Biar tidak menghalangi klik
+    gif.style.zIndex = "9999"; // Pastikan di atas elemen lain
 
-    document.querySelector("tabs-list").appendChild(img);
+    document.body.appendChild(gif);
+
+    document.addEventListener("mousemove", (e) => {
+        gif.style.left = `${e.pageX + 10}px`; // Offset sedikit dari kursor
+        gif.style.top = `${e.pageY + 10}px`;
+    });
 });
